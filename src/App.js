0226d236374b619f9f97f9183ed604fa354c40df
SomeRandomDude1897 <sokhanivan@yandex.ru>
2024-09-24 10:59:43 +0300

    Commit after the divergent branches wont merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css"
+import "./index.css"
 import StartScreen from './StartScreen';
 import About from './About';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Skills from "./Skills";
 import Contacts from "./Contacs";
 
 function App() {
+  const [fade, SetFade] = useState(false);
   const aboutRef = useRef(null);
   const skillsRef = useRef(null);
   const contactsRef = useRef(null);
@@ -13,11 +14,18 @@ function App() {
     ref.current.scrollIntoView({behavior: "smooth"});
   }
   return (
-    <div className="app-class">
-      <StartScreen ContactsScroll={() => {scrollTo(contactsRef)}} AboutScroll={() => {scrollTo(aboutRef)}} SkillsScroll={() => {scrollTo(skillsRef)}}></StartScreen>
-      <About Reference={aboutRef}></About>
-      <Skills Reference={skillsRef}></Skills>
-      <Contacts Reference={contactsRef}></Contacts>
+    <div>
+      <div id="text-for-mobiles">Мобильная версия сайта в разработке</div>
+      <div
+      onLoad={() => {SetFade(true)}}
+      className={"app " + (fade ? "fade-in" : "fade-out")}>
+        <StartScreen
+        fade={fade}
+        ContactsScroll={() => {scrollTo(contactsRef)}} AboutScroll={() => {scrollTo(aboutRef)}} SkillsScroll={() => {scrollTo(skillsRef)}}></StartScreen>
+        <About Reference={aboutRef}></About>
+        <Skills Reference={skillsRef}></Skills>
+        <Contacts Reference={contactsRef}></Contacts>
+      </div>
     </div>
   );
 }
--- src/Contacs.js
+++ src/Contacs.js
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import PointPhoto from "./pictures/pointing-image.png"
+import GithubImage from "./pictures/github.png"
+import TelegramImage from "./pictures/telegram.png"
+import EmailImage from "./pictures/at_sign.png"
 
 const Contacts = (props) => {
     const [copiedText, SetCopiedText] = useState(false);
@@ -8,33 +11,25 @@ const Contacts = (props) => {
         SetCopiedText(true);
         setTimeout(() => {SetCopiedText(false)}, 1000)
     }
+    const goToURL = (path) => {
+        window.location.href = path;
+      };
+
     return ( 
         <div>
             <div ref={props.Reference} className="contacts-box">
                 <div className="headed-list">
                     <div className="content-capital-text">Как со мной связаться</div>
-                    <ul style={{listStyleType: 'none'}}>
-                        <br/>
-                        <li> 
-                            <div className="content-text">Email:</div>
-                            <button onClick={() => {copyToClipboard("sokhanivan@yandex.ru")}} className="content-copyable content-text">sokhanivan@yandex.ru</button>
-                        </li>
-                        <li> 
-                            <div className="content-text">Telegram:</div>
-                            <a href="https://t.me/LeCirno" className="content-copyable content-text">@LeCirno</a>
-                        </li>
-                        <li> 
-                        <div className="content-text">Github:</div>
-                        <a href="https://github.com/SomeRandomDude1897" className="content-copyable content-text">https://github.com/SomeRandomDude1897</a>
-                        </li>
-                        <li> 
-                            <div className="content-text">Телефон:</div>
-                            <button onClick={() => {copyToClipboard("+79509955770")}} className="content-copyable content-text">+79509955770</button>
-                        </li>
-                        <br/>
-                        <br/>
-                        <div className={"content-text " + (copiedText ? "fade-in" : "fade-out")} id="text-copied-popup">Текст скопирован в буфер обмена</div>
-                    </ul>
+                    <br/>
+                    <br/>
+                    <div id="contacts-container">
+                        <img onClick={() => {copyToClipboard("sokhanivan@yandex.ru")}} src={EmailImage} alt="моя почта" className="contacts-icon"></img>
+                        <img onClick={() => {goToURL("https://t.me/LeCirno")}} src={TelegramImage} alt="мой телеграм" className="contacts-icon" ></img>
+                        <img onClick={() => {goToURL("https://github.com/SomeRandomDude1897")}} src={GithubImage} alt="мой гитхаб" className="contacts-icon"></img>
+                    </div>
+                    <br/>
+                    <br/>
+                    <div className={"content-text " + (copiedText ? "fade-in" : "fade-out")} id="text-copied-popup">Email скопирован в буфер обмена</div>
                 </div>
                 <img className="contacts-pic" 
                 style={{marginLeft: "10%", marginTop: "10%"}} src = {PointPhoto} alt = "здесь фотка меня показывающего налево"></img>
